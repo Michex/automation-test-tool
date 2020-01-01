d2cd8b4b526cf4b855a83a8485f1eeae53629440
Szymon Sasin <szymex@gmail.com>
2020-01-01 17:38:30 +0200

    added unit tests for Helper class

---
+++ src/test/java/helper/HelperTest.java
@@ -0,0 +1,23 @@
+package helper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelperTest {
+
+    @Test
+    public void shouldConvertCamelCaseToNormal() {
+        assertEquals("Dupa Blada", Helper.convertCamelCasesToNormal("DupaBlada"));
+        assertEquals("Dupa", Helper.convertCamelCasesToNormal("Dupa"));
+        assertEquals("Dupa", Helper.convertCamelCasesToNormal("dupa"));
+
+        //corner cases
+        assertEquals("", Helper.convertCamelCasesToNormal(""));
+        assertEquals("A", Helper.convertCamelCasesToNormal("A"));
+        assertEquals("A", Helper.convertCamelCasesToNormal("a"));
+        assertEquals("ABC", Helper.convertCamelCasesToNormal("ABC"));
+        assertEquals("AB Cd EF", Helper.convertCamelCasesToNormal("ABCdEF"));
+        assertEquals("Äbb Öoo", Helper.convertCamelCasesToNormal("ÄbbÖoo"));
+    }
+}
